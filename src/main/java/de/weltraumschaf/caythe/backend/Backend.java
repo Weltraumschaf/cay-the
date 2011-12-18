6cfce88a1f760e9cf4823f02d526fe41be34e369
Weltraumschaf <ich@weltraumschaf.de>
2011-12-18 18:14:02 +0100

    Add abstract backend class to framework.

---
+++ src/main/java/de/weltraumschaf/caythe/backend/Backend.java
@@ -0,0 +1,44 @@
+package de.weltraumschaf.caythe.backend;
+
+import de.weltraumschaf.caythe.intermediate.ICode;
+import de.weltraumschaf.caythe.intermediate.SymbolTable;
+import de.weltraumschaf.caythe.message.Message;
+import de.weltraumschaf.caythe.message.MessageHandler;
+import de.weltraumschaf.caythe.message.MessageListener;
+import de.weltraumschaf.caythe.message.MessageProducer;
+
+/**
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ * @license http://www.weltraumschaf.de/the-beer-ware-license.txt THE BEER-WARE LICENSE
+ */
+public abstract class Backend implements MessageProducer {
+
+    protected static MessageHandler messageHandler;
+
+    static {
+        messageHandler = new MessageHandler();
+    }
+
+    protected SymbolTable symTab;
+    protected ICode iCode;
+
+    public abstract void process(ICode iCode, SymbolTable symTab) throws Exception;
+
+    @Override
+    public void addMessageListener(MessageListener listener) {
+        messageHandler.addListener(listener);
+    }
+
+    @Override
+    public void removeMessageListener(MessageListener listener) {
+        messageHandler.removeListener(listener);
+    }
+
+    @Override
+    public void sendMessage(Message message) {
+        messageHandler.sendMessage(message);
+    }
+
+
+}
