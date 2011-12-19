8afee9706bcf14946838a5e218e15ccd8be7669e
Weltraumschaf <ich@weltraumschaf.de>
2011-12-19 09:21:35 +0100

    Add stub classes for Pascal lexer.

---
+++ src/main/java/de/weltraumschaf/caythe/frontend/pascal/PascalTokenType.java
@@ -0,0 +1,17 @@
+package de.weltraumschaf.caythe.frontend.pascal;
+
+import de.weltraumschaf.caythe.frontend.TokenType;
+import java.util.HashSet;
+import java.util.Hashtable;
+
+/**
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ * @license http://www.weltraumschaf.de/the-beer-ware-license.txt THE BEER-WARE LICENSE
+ */
+public class PascalTokenType implements TokenType {
+
+    public static final HashSet RESERVED_WORDS = null;
+    public static final Hashtable SPECIAL_SYMBOLS = null;
+
+}
---
+++ src/main/java/de/weltraumschaf/caythe/frontend/pascal/tokens/PascalErrorToken.java
@@ -0,0 +1,17 @@
+package de.weltraumschaf.caythe.frontend.pascal.tokens;
+
+import de.weltraumschaf.caythe.frontend.Source;
+import de.weltraumschaf.caythe.frontend.pascal.PascalToken;
+
+/**
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ * @license http://www.weltraumschaf.de/the-beer-ware-license.txt THE BEER-WARE LICENSE
+ */
+public class PascalErrorToken extends PascalToken {
+
+    public PascalErrorToken(Source source) throws Exception {
+        super(source);
+    }
+    
+}
