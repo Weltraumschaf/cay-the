524cc3e110c620fab4218ee642e543bd92bd4c07
Weltraumschaf <ich@weltraumschaf.de>
2011-12-20 19:22:12 +0100

    Add key enum.

---
+++ src/main/java/de/weltraumschaf/caythe/intermediate/symboltableimpl/SymbolTableKeyImpl.java
@@ -0,0 +1,20 @@
+package de.weltraumschaf.caythe.intermediate.symboltableimpl;
+
+import de.weltraumschaf.caythe.intermediate.SymbolTableKey;
+
+/**
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ * @license http://www.weltraumschaf.de/the-beer-ware-license.txt THE BEER-WARE LICENSE
+ */
+public enum SymbolTableKeyImpl implements SymbolTableKey {
+    // Constant.
+    CONSTANT_VALUE,
+
+    // Procedure or function.
+    ROUTINE_CODE, ROUTINE_SYMTAB, ROUTINE_ICODE,
+    ROUTINE_PARMS, ROUTINE_ROUTINES,
+
+    // Variable or record field value.
+    DATA_VALUE
+}
