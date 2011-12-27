108d8498e65b606b076c09539d5548b7a157e9f1
Weltraumschaf <ich@weltraumschaf.de>
2011-12-27 17:25:13 +0100

    Fix the interface.

--- src/main/java/de/weltraumschaf/caythe/intermediate/TypeSpec.java
+++ src/main/java/de/weltraumschaf/caythe/intermediate/TypeSpec.java
@@ -6,11 +6,11 @@ package de.weltraumschaf.caythe.intermediate;
  * @license http://www.weltraumschaf.de/the-beer-ware-license.txt THE BEER-WARE LICENSE
  */
 public interface TypeSpec {
-    public TypForm getForm();
+    public TypeForm getForm();
     public void setIdentifer(SymbolTableEntry identifer);
     public SymbolTableEntry getIdentifier();
-    public void setAtrribute(TypeKey key, Object value);
-    public Object getAttribute();
+    public void setAttribute(TypeKey key, Object value);
+    public Object getAttribute(TypeKey key);
     public boolean isPascalString();
     public TypeSpec baseType();
 }
