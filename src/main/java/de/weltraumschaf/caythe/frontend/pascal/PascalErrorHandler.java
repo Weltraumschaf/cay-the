6b80c0f2a55c8aa430d83e730c56f32e49281254
Weltraumschaf <ich@weltraumschaf.de>
2012-01-02 16:36:54 +0100

    Add method to flag string messages.

--- src/main/java/de/weltraumschaf/caythe/frontend/pascal/PascalErrorHandler.java
+++ src/main/java/de/weltraumschaf/caythe/frontend/pascal/PascalErrorHandler.java
@@ -17,17 +17,21 @@ public class PascalErrorHandler {
 
     private static int errorCount = 0;
 
-    public void flag(Token token, PascalErrorCode errorCode, Parser parser) {
-	parser.sendMessage(new Message(SYNTAX_ERROR, new Object[] {
+    public void flag(Token token, String errorMessage, Parser parser) {
+        parser.sendMessage(new Message(SYNTAX_ERROR, new Object[] {
 	    token.getLineNumber(),
 	    token.getPosition(),
 	    token.getText(),
-	    errorCode.toString()
+	    errorMessage
 	}));
 
 	if (++errorCount > MAX_ERRORS) {
 	    abortTranslation(TOO_MANY_ERRORS, parser);
 	}
+    }
+
+    public void flag(Token token, PascalErrorCode errorCode, Parser parser) {
+	flag(token, errorCode.toString(), parser);
     }
 
     public void abortTranslation(PascalErrorCode errorCode, Parser parser) {
