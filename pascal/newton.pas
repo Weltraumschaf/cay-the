8d207e62a71be4536598073aea7f9fac341e6ddf
Weltraumschaf <ich@weltraumschaf.de>
2012-01-05 21:16:25 +0100

    More test code.

--- pascal/newton.pas
+++ pascal/newton.pas
@@ -1,11 +1,11 @@
-PROGRAM newton (input, output);
+PROGRAM newton;
 
 CONST
-    EPSILON = 1e-6;
+    epsilon = 1e-6;
 
 VAR
     number       : integer;
-    root, sqRoot : real;
+    root, sqroot : real;
 
 BEGIN
     REPEAT
@@ -20,17 +20,17 @@ BEGIN
             writeln('*** ERROR:  number < 0');
         END
         ELSE BEGIN
-            sqRoot := sqrt(number);
-            writeln(number:12, sqRoot:12:6);
+            sqroot := sqrt(number);
+            writeln(number:12, sqroot:12:6);
             writeln;
 
             root := 1;
             REPEAT
                 root := (number/root + root)/2;
                 writeln(root:24:6,
-                        100*abs(root - sqRoot)/sqRoot:12:2,
+                        100*abs(root - sqroot)/sqroot:12:2,
                         '%')
-            UNTIL abs(number/sqr(root) - 1) < EPSILON;
+            UNTIL abs(number/sqr(root) - 1) < epsilon;
         END
     UNTIL number = 0
 END.
