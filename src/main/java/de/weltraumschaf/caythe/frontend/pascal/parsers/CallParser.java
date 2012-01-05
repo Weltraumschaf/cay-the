cdf8e8506fdb78d502f8ae98fde880929bed22de
Weltraumschaf <ich@weltraumschaf.de>
2012-01-05 17:22:35 +0100

    Implement call parsers for declared functions/procedures and predefined ones.

--- src/main/java/de/weltraumschaf/caythe/frontend/pascal/parsers/CallParser.java
+++ src/main/java/de/weltraumschaf/caythe/frontend/pascal/parsers/CallParser.java
@@ -1,6 +1,13 @@
 package de.weltraumschaf.caythe.frontend.pascal.parsers;
 
+import de.weltraumschaf.caythe.frontend.Token;
 import de.weltraumschaf.caythe.frontend.pascal.PascalTopDownParser;
+import de.weltraumschaf.caythe.intermediate.CodeNode;
+import de.weltraumschaf.caythe.intermediate.RoutineCode;
+import de.weltraumschaf.caythe.intermediate.SymbolTableEntry;
+
+import static de.weltraumschaf.caythe.intermediate.symboltableimpl.RoutineCodeImpl.*;
+import static de.weltraumschaf.caythe.intermediate.symboltableimpl.SymbolTableKeyImpl.ROUTINE_CODE;
 
 /**
  *
@@ -13,4 +20,22 @@ public class CallParser extends StatementParser {
         super(parent);
     }
 
+    @Override
+    public CodeNode parse(Token token) throws Exception {
+        SymbolTableEntry pfId      = symbolTableStack.lookup(token.getText().toLowerCase());
+        RoutineCode routineCode    = (RoutineCode) pfId.getAttribute(ROUTINE_CODE);
+        StatementParser callParser;
+
+        if ((routineCode == DECLARED) || (routineCode == FORWARD)) {
+            callParser = new CallDeclaredParser(this);
+        } else {
+            callParser = new CallStandardParser(this);
+        }
+
+        return callParser.parse(token);
+    }
+
+    protected CodeNode parseActualParameters(Token token, SymbolTableEntry pfId, boolean isDecalred, boolean isReadReadln, boolean isWriteWriteln) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 }
