3f7feba351e4ad4a182c7877d1e6a92df00a2002
Weltraumschaf <ich@weltraumschaf.de>
2012-01-10 21:55:50 +0100

    Implement remaining methods in memory factory.

--- src/main/java/de/weltraumschaf/caythe/backend/interpreter/MemoryFactory.java
+++ src/main/java/de/weltraumschaf/caythe/backend/interpreter/MemoryFactory.java
@@ -1,6 +1,12 @@
 package de.weltraumschaf.caythe.backend.interpreter;
 
+import de.weltraumschaf.caythe.backend.interpreter.memoryimpl.ActivationRecordImpl;
+import de.weltraumschaf.caythe.backend.interpreter.memoryimpl.CellImpl;
+import de.weltraumschaf.caythe.backend.interpreter.memoryimpl.MemoryMapImpl;
+import de.weltraumschaf.caythe.backend.interpreter.memoryimpl.RuntimeDisplayImpl;
+import de.weltraumschaf.caythe.backend.interpreter.memoryimpl.RuntimeStackImpl;
 import de.weltraumschaf.caythe.intermediate.SymbolTable;
+import de.weltraumschaf.caythe.intermediate.SymbolTableEntry;
 
 /**
  *
@@ -9,16 +15,24 @@ import de.weltraumschaf.caythe.intermediate.SymbolTable;
  */
 public class MemoryFactory {
 
+    public static RuntimeStack createRuntimeStack() {
+        return new RuntimeStackImpl();
+    }
+
     public static RuntimeDisplay createRuntimeDisplay() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return new RuntimeDisplayImpl();
+    }
+
+    public static ActivationRecord createActivationRecord(SymbolTableEntry routineId) {
+        return new ActivationRecordImpl(routineId);
     }
 
     public static MemoryMap createMemoryMap(SymbolTable symTab) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return new MemoryMapImpl(symTab);
     }
 
-    public static Cell createCell(Object o) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public static Cell createCell(Object value) {
+        return new CellImpl(value);
     }
 
 }
