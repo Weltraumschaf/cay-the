36860d352255e79ee088aee095e838d4f6a2258a
Weltraumschaf <ich@weltraumschaf.de>
2012-04-10 00:35:19 +0200

    Implemented skipWhitespace().

--- src/main/java/de/weltraumschaf/caythe/frontend/caythe/CayTheScanner.java
+++ src/main/java/de/weltraumschaf/caythe/frontend/caythe/CayTheScanner.java
@@ -2,6 +2,7 @@ package de.weltraumschaf.caythe.frontend.caythe;
 
 import de.weltraumschaf.caythe.frontend.Scanner;
 import de.weltraumschaf.caythe.frontend.Source;
+import static de.weltraumschaf.caythe.frontend.Source.EOF;
 import de.weltraumschaf.caythe.frontend.Token;
 
 /**
@@ -20,4 +21,39 @@ public class CayTheScanner extends Scanner {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * Skips white spaces and comments.
+     *
+     * @throws Exception
+     */
+    private void skipWhitespace() throws Exception {
+        char currentChar = currentChar();
+
+        while (Character.isWhitespace(currentChar) || ( '/' == currentChar )) {
+            // Start of a comment?
+            if ('/' == currentChar) {
+                char nextChar = peakChar();
+
+                if ('/' == nextChar) { // single line comment
+                    skipToNextLine();
+                }
+                else if ('*' == nextChar) { // multi line comment
+                    do {
+                        currentChar = nextChar(); // Consume comment chars.
+                    } while (('*' != currentChar) && ('/' != peakChar()) && (EOF != currentChar));
+
+                    // Found closing '*/'?
+                    if ('*' == currentChar) {
+                        currentChar = nextChar(); // Consumes the '*'.
+                        if ('/' == currentChar) {
+                            currentChar = nextChar(); // Consumes the '/'.
+                        }
+                    }
+                }
+            } // Not a comment.
+            else {
+                currentChar = nextChar();
+            }
+        }
+    }
 }
