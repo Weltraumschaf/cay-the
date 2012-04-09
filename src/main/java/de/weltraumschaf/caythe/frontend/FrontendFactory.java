6e286f9e1e7e0bcdd4425a0eb718e7e77354ebbf
Weltraumschaf <ich@weltraumschaf.de>
2012-04-10 00:00:59 +0200

    Organize imports, remove language enum and add cay-the.

--- src/main/java/de/weltraumschaf/caythe/frontend/FrontendFactory.java
+++ src/main/java/de/weltraumschaf/caythe/frontend/FrontendFactory.java
@@ -1,7 +1,10 @@
 package de.weltraumschaf.caythe.frontend;
 
-import de.weltraumschaf.caythe.frontend.pascal.PascalTopDownParser;
+import de.weltraumschaf.caythe.Language;
+import de.weltraumschaf.caythe.frontend.caythe.CayTheScanner;
+import de.weltraumschaf.caythe.frontend.caythe.CayTheTopDownParser;
 import de.weltraumschaf.caythe.frontend.pascal.PascalScanner;
+import de.weltraumschaf.caythe.frontend.pascal.PascalTopDownParser;
 
 /**
  *
@@ -9,20 +12,33 @@ import de.weltraumschaf.caythe.frontend.pascal.PascalScanner;
  * @license http://www.weltraumschaf.de/the-beer-ware-license.txt THE BEER-WARE LICENSE
  */
 public class FrontendFactory {
-    public enum Language {
-        PASCAL
-    }
 
     public enum Type {
         TOP_DOWN
     }
 
+    public static Parser createParser(Language lang, Source source) throws Exception {
+        return createParser(lang, Type.TOP_DOWN, source);
+    }
+
     public static Parser createParser(Language lang, Type type, Source source) throws Exception {
-        if (Language.PASCAL == lang && Type.TOP_DOWN == type) {
-            Scanner scanner = new PascalScanner(source);
-            return new PascalTopDownParser(scanner);
-        } else {
-            throw new Exception(String.format("Invalid language %s or type %s!", lang, type));
+        switch (lang) {
+            case PASCAL: {
+                Scanner scanner = new PascalScanner(source);
+
+                if (Type.TOP_DOWN == type) {
+                    return new PascalTopDownParser(scanner);
+                } else {
+                    throw new Exception("Other pascal parsers than TOP_DOWN are not implemented, yet!");
+                }
+            }
+            case CAYTHE: {
+                Scanner scanner = new CayTheScanner(source);
+                return new CayTheTopDownParser(scanner);
+            }
+            default: {
+                throw new Exception(String.format("Invalid language %s or type %s!", lang, type));
+            }
         }
     }
 }
