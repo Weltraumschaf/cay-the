43f3a42786fc1eb0797780d0b9fa621271492a26
Weltraumschaf <ich@weltraumschaf.de>
2012-04-10 13:14:04 +0200

    Test source reading.

--- src/test/java/de/weltraumschaf/caythe/frontend/SourceTest.java
+++ src/test/java/de/weltraumschaf/caythe/frontend/SourceTest.java
@@ -2,13 +2,14 @@ package de.weltraumschaf.caythe.frontend;
 
 import de.weltraumschaf.caythe.message.Message;
 import de.weltraumschaf.caythe.message.MessageHandler;
-import de.weltraumschaf.caythe.message.MessageListener;
 import de.weltraumschaf.caythe.message.MessageType;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
 import org.junit.Ignore;
 import org.junit.Test;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  *
@@ -16,6 +17,13 @@ import static org.mockito.Mockito.times;
  * @license http://www.weltraumschaf.de/the-beer-ware-license.txt THE BEER-WARE LICENSE
  */
 public class SourceTest {
+
+    private static final String FIXTURE_DIR = "/de/weltraumschaf/caythe/frontend";
+
+    private Source createSourceFromFixture(String fixtureFile) throws FileNotFoundException, URISyntaxException {
+        URL resource = getClass().getResource(FIXTURE_DIR + '/' + fixtureFile);
+        return new Source(new BufferedReader(new FileReader(new File(resource.toURI()))));
+    }
 
     @Test public void testMessageProducer() {
         Message msg1 = new Message(MessageType.CALL, "msg1");
@@ -54,8 +62,32 @@ public class SourceTest {
 
     }
 
-    @Ignore
-    @Test public void testReadWholeSource() {
+    @Test public void testReadWholeSource() throws Exception {
+        Source sut = createSourceFromFixture("empty_source");
+        assertEquals(-2, sut.getCurrentPos());
+        assertEquals(0, sut.getLineNumber());
+        assertEquals(Source.EOF, sut.currentChar());
+        assertEquals(0, sut.getCurrentPos());
+        assertEquals(0, sut.getLineNumber());
 
+        sut = createSourceFromFixture("one_line_source");
+        assertEquals(-2, sut.getCurrentPos());
+        assertEquals(0, sut.getLineNumber());
+
+        char[] chars = "Source with one line".toCharArray();
+        assertEquals('S', sut.currentChar());
+        assertEquals(0, sut.getCurrentPos());
+        assertEquals(1, sut.getLineNumber());
+        for (int i = 1; i < chars.length; ++i) {
+            assertEquals("iteration " + i, chars[i], sut.nextChar());
+            assertEquals("iteration " + i, chars[i], sut.currentChar());
+            assertEquals(i, sut.getCurrentPos());
+            assertEquals(1, sut.getLineNumber());
+
+        }
+        assertEquals(Source.EOL, sut.nextChar());
+        assertEquals(Source.EOF, sut.nextChar());
+        assertEquals(0, sut.getCurrentPos());
+        assertEquals(1, sut.getLineNumber());
     }
 }
