83bced5537c13a8e3318e99d62c4e0ef59c38fe0
Weltraumschaf <ich@weltraumschaf.de>
2012-04-11 14:08:22 +0200

    Implement CayThe number token.

---
+++ src/test/java/de/weltraumschaf/caythe/frontend/caythe/tokens/CayTheNumberTokenTest.java
@@ -0,0 +1,17 @@
+package de.weltraumschaf.caythe.frontend.caythe.tokens;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public class CayTheNumberTokenTest {
+
+    @Ignore
+    @Test public void extract() {
+
+    }
+
+}
