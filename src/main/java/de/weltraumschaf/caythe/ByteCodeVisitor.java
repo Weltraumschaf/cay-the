aae0281256dd5ff8a616f8a35f9e5b022c97bf03
Sven Strittmatter <sven.strittmatter@iteratec.de>
2015-12-05 23:52:29 +0100

    Implement first visit method for compilation unit.

--- src/main/java/de/weltraumschaf/caythe/ByteCodeVisitor.java
+++ src/main/java/de/weltraumschaf/caythe/ByteCodeVisitor.java
@@ -1,12 +1,28 @@
 package de.weltraumschaf.caythe;
 
+import org.antlr.v4.runtime.tree.ParseTree;
+
 /**
  */
 final class ByteCodeVisitor extends CayTheBaseVisitor<Programm> {
+
+    private final SymbolTable table = new SymbolTable();
 
     @Override
     protected Programm defaultResult() {
         return Programm.EMPTY;
     }
+
+    @Override
+    public Programm visitCompilationUnit(final CayTheParser.CompilationUnitContext ctx) {
+        final ParseTree compilationUnit = ctx.getChild(0);
+
+        if (null == compilationUnit) {
+            return defaultResult();
+        }
+
+        return visit(compilationUnit);
+    }
+
 
 }
