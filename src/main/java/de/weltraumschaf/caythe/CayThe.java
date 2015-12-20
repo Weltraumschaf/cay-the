280fac07bb5e1061c9f68e66bcca1e0c42d2ade7
Sven Strittmatter <sven.strittmatter@iteratec.de>
2015-12-20 17:29:22 +0100

    Fix version file path.

--- src/main/java/de/weltraumschaf/caythe/CayThe.java
+++ src/main/java/de/weltraumschaf/caythe/CayThe.java
@@ -10,7 +10,7 @@ public final class CayThe {
     public static final String DEFAULT_ENCODING = "utf-8";
     public static final String DEFAULT_NEWLINE = String.format("%n");
     public static final String BASE_PACKAGE = "de.weltraumschaf.caythe";
-    public static final String BASE_PACKAGE_DIR = "/" + BASE_PACKAGE.replaceAll(".", "/");
+    public static final String BASE_PACKAGE_DIR = "/" + BASE_PACKAGE.replaceAll("\\.", "/");
     public static final String ENV_DEBUG = "CAYTHE_DEBUG";
 
 }
--- src/test/java/de/weltraumschaf/caythe/CayTheTest.java
+++ src/test/java/de/weltraumschaf/caythe/CayTheTest.java
@@ -6,12 +6,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
+ * Tests for {@link CayThe}.
  */
 public class CayTheTest {
 
     @Test
-    public void testSomeMethod() {
-//        assertThat(true > true, is(true));
+    public void BASE_PACKAGE_DIR() {
+        assertThat(CayThe.BASE_PACKAGE_DIR, is("/de/weltraumschaf/caythe"));
     }
 
 }
