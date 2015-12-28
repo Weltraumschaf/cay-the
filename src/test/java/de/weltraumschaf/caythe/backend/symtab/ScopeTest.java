e59cbbb7a46108d1df926e108325b7fbe4d77b4e
Sven Strittmatter <sven.strittmatter@iteratec.de>
2015-12-28 16:47:34 +0100

    Add test for null scope.

---
+++ src/test/java/de/weltraumschaf/caythe/backend/symtab/ScopeTest.java
@@ -0,0 +1,24 @@
+
+package de.weltraumschaf.caythe.backend.symtab;
+
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ */
+public class ScopeTest {
+
+    @Test
+    public void defaultsOfNullObject() {
+        final Scope sut = Scope.NULL;
+
+        assertThat(sut.getScopeName(), is("NULL"));
+        assertThat(sut.getEnclosing(), is(Scope.NULL));
+        assertThat(sut.hasEnclosing(), is(false));
+        assertThat(sut.isDefined("any"), is(false));
+        assertThat(sut.resolve("any"), is(Symbol.NULL));
+        assertThat(sut.load(Symbol.NULL), is(Value.NIL));
+    }
+
+}
