96917b9d98dd3f6942356360482fc8bc034feca5
Sven Strittmatter <ich@weltraumschaf.de>
2016-12-19 23:48:46 +0100

    Add missing package info files.

---
+++ src/main/java/de/weltraumschaf/caythe/core/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains core classes used by all others.
+ *
+ * @since 1.0.0
+ * @author Sven Strittmatter &lt;weltraumschaf@googlemail.com&gt;
+ */
+package de.weltraumschaf.caythe.core;
