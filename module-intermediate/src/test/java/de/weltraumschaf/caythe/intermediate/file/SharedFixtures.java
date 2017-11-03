304d9db77984c1fa369edd59b6b5c9b1ed8b598c
Sven Strittmatter <ich@weltraumschaf.de>
2017-11-03 17:35:23 +0100

    Fix test fixture

--- module-intermediate/src/test/java/de/weltraumschaf/caythe/intermediate/file/SharedFixtures.java
+++ module-intermediate/src/test/java/de/weltraumschaf/caythe/intermediate/file/SharedFixtures.java
@@ -19,5 +19,5 @@ final class SharedFixtures {
                 new Coordinate("org.foobar", "snafu", new Version(3, 0, 0))));
     static final Module MODULE_FIXTURE = new Module(MANIFEST_FIXTURE, Collections.emptyList(), Collections.emptyList());
     static final String JSON_FIXTURE =
-        "{\"manifest\":{\"coordinate\":{\"group\":\"de.weltraumschaf\",\"artifact\":\"caythe\",\"version\":{\"major\":1,\"minor\":0,\"patch\":0,\"identifiers\":\"\"}},\"namespace\":\"de.weltraumschaf.caythe\",\"imports\":[{\"group\":\"com.foobar\",\"artifact\":\"baz\",\"version\":{\"major\":1,\"minor\":2,\"patch\":3,\"identifiers\":\"\"}},{\"group\":\"org.foobar\",\"artifact\":\"snafu\",\"version\":{\"major\":3,\"minor\":0,\"patch\":0,\"identifiers\":\"\"}}]},\"units\":[],\"resources\":[]}";
+        "{\"manifest\":{\"coordinate\":{\"group\":\"de.weltraumschaf\",\"artifact\":\"caythe\",\"version\":{\"major\":1,\"minor\":0,\"patch\":0,\"identifiers\":\"\"}},\"namespace\":\"de.weltraumschaf.caythe\",\"imports\":[{\"group\":\"com.foobar\",\"artifact\":\"baz\",\"version\":{\"major\":1,\"minor\":2,\"patch\":3,\"identifiers\":\"\"}},{\"group\":\"org.foobar\",\"artifact\":\"snafu\",\"version\":{\"major\":3,\"minor\":0,\"patch\":0,\"identifiers\":\"\"}}]},\"types\":[],\"resources\":[]}";
 }
