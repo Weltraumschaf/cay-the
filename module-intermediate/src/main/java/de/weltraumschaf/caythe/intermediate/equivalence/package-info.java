eb0fa19097db02c703f7b747b713a8877fa187b1
Sven Strittmatter <ich@weltraumschaf.de>
2018-01-17 18:14:33 +0100

    Add missing package infos

---
+++ module-intermediate/src/main/java/de/weltraumschaf/caythe/intermediate/equivalence/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains classes to probe the equivalence of intermediate model objects.
+ *
+ * @author Sven Strittmatter &lt;weltraumschaf@googlemail.com&gt;
+ * @since 1.0.0
+ */
+package de.weltraumschaf.caythe.intermediate.equivalence;
---
+++ module-intermediate/src/main/java/de/weltraumschaf/caythe/intermediate/model/ast/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the parts of the intermediate model which represent the code level stuff.
+ *
+ * @author Sven Strittmatter &lt;weltraumschaf@googlemail.com&gt;
+ * @since 1.0.0
+ */
+package de.weltraumschaf.caythe.intermediate.model.ast;
